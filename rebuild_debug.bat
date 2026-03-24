866d07d7c15fe41ac577c18ab6633426ae8eb221
friendan <407583729@qq.com>
2026-03-24 23:54:18 +0800

    init

---
+++ rebuild_debug.bat
@@ -0,0 +1,12 @@
+﻿@echo off
+chcp 65001 >nul
+cls
+@echo 正在强制重新编译 Debug 版本...
+cmake --build build --config Debug -- /t:Rebuild
+@echo.
+@echo ------------------------------
+@echo 编译完成！窗口保持打开可输入命令
+@echo ------------------------------
+@echo.
+@cmd /k
+
